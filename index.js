3c2b87914abd8bc245628863843f8f7cf7f0ce90
ckorley4 <ckorley4uo@yahoo.com>
2023-11-28 03:51:52 -0500

    Arithmetic Lab

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function increment(a) {
+  return ++a
+}
+
+function decrement(b) {
+  return --b
+}
+
+function makeInt(string) {
+  return parseInt(string, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
